2fd6a6c45fe97ab849f6bf0f200f08ffd7ab6d8e
Joey Herguth <oitzujoey@gmail.com>
2019-12-25 02:10:33 -0800

    Fix some formatting in cpu-adelie.c

--- adelie-gcc/binutils-2.32/bfd/cpu-adelie.c
+++ adelie-gcc/binutils-2.32/bfd/cpu-adelie.c
@@ -25,19 +25,18 @@
 #include "libbfd.h"
 
 
-const bfd_arch_info_type bfd_adelie_arch =
-  {
-    32,		      /* 32 bits in a word.  */
-    32,		      /* 32 bits in an address.  */
-    8,		      /* 8 bits in a byte.  */
-    bfd_arch_adelie,   /* enum bfd_architecture arch.  */
-    bfd_mach_adelie,    /* Machine value, used to distinguish between adelie variants.  */
-    "adelie",	      /* Arch name.  */
-    "adelie",	      /* Printable name.  */
-    2,	/* log2 of unsigned int section alignment in bytes */
-    TRUE,             /* True if this is the default machine for the architecture.  */
+const bfd_arch_info_type bfd_adelie_arch = {
+    32,                     /* 32 bits in a word.  */
+    32,                     /* 32 bits in an address.  */
+    8,                      /* 8 bits in a byte.  */
+    bfd_arch_adelie,        /* enum bfd_architecture arch.  */
+    bfd_mach_adelie,        /* Machine value, used to distinguish between adelie variants.  */
+    "adelie",               /* Arch name.  */
+    "adelie",               /* Printable name.  */
+    2,                      /* log2 of unsigned int section alignment in bytes */
+    TRUE,                   /* True if this is the default machine for the architecture.  */
     bfd_default_compatible,
-    bfd_default_scan, /* Function to call to determine if two different architectures are compatible.  */
-    bfd_arch_default_fill, /* Function to call to determine if a given string matches this architecture.  */ 
-    NULL       /*  Next in list. Other "bfd_arch_info"s would normally go here */
-  };
+    bfd_default_scan,       /* Function to call to determine if two different architectures are compatible.  */
+    bfd_arch_default_fill,  /* Function to call to determine if a given string matches this architecture.  */ 
+    NULL                    /*  Next in list. Other "bfd_arch_info"s would normally go here */
+};

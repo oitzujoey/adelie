9164bb5e615e61125ea0b5c39a631827d5c5ffb0
Joey Herguth <oitzujoey@gmail.com>
2020-02-11 20:33:28 -0600

    Add build scripts to helper-scripts directory

---
+++ toolchain/helper-scripts/test_as.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "--> AS <--"
+gas/as-new -o add-test.o ../../../adelie/asm/ldi.m-test.s
+echo ""
+echo "--> LD <--"
+ld/ld-new -o add-test add-test.o
+echo ""
+echo "--> OBJDUMP: -r -d *.o <--"
+binutils/objdump -r -d add-test.o
+echo ""
+echo "--> OBJDUMP: -r -d * <--"
+binutils/objdump -r -d add-test
